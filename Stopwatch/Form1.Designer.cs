04cc5070762b258e1a8442cef361da11bb3ac573
cjdenio <34525547+cjdenio@users.noreply.github.com>
2018-04-15 18:57:42 -0400

    Add project files.

---
+++ Stopwatch/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clockText = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // clockText
+            // 
+            this.clockText.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clockText.Location = new System.Drawing.Point(150, 100);
+            this.clockText.Name = "clockText";
+            this.clockText.Size = new System.Drawing.Size(291, 63);
+            this.clockText.TabIndex = 0;
+            this.clockText.Text = "00:00";
+            this.clockText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.AutoSize = true;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(237, 251);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(117, 62);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(360, 266);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(103, 47);
+            this.resetButton.TabIndex = 2;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(590, 376);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.clockText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label clockText;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Stopwatch/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Stopwatch
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "C:/Users/cjden/Desktop/clock.txt";
+        int rawTimerValue = 0;
+        int minutes = 0;
+        int seconds;
+        string fullClockValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fullClockValue = "00:00";
+            if (File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+            File.WriteAllText(filePath, fullClockValue);
+        }
+        
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            rawTimerValue++;
+            if(seconds < 59)
+            {
+                seconds++;
+            }
+            else
+            {
+                seconds = 0;
+                minutes++;
+            }
+            //Ternary operator used to add a 0 before values with only one character
+            fullClockValue = (minutes.ToString().Length == 1 ? "0" + minutes.ToString() : minutes.ToString()) + ":" + (seconds.ToString().Length == 1 ? "0"+seconds.ToString() : seconds.ToString());
+            clockText.Text = fullClockValue;
+            File.WriteAllText("C:/Users/cjden/Desktop/clock.txt", fullClockValue);
+        }
+        bool isActive = false;
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            isActive = !isActive;
+            switch (isActive)
+            {
+                case true: startButton.Text = "Stop";
+                    timer.Start();
+                    break;
+                default: startButton.Text = "Start";
+                    timer.Stop();
+                    break;
+            }
+            //startButton.Text = "";
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            isActive = false;
+            startButton.Text = "Start";
+            rawTimerValue = 0;
+            minutes = 00;
+            seconds = 00;
+            timer.Stop();
+            fullClockValue = "00:00";
+            clockText.Text = fullClockValue;
+            File.WriteAllText("C:/Users/cjden/Desktop/clock.txt", fullClockValue);
+        }
+    }
+}
